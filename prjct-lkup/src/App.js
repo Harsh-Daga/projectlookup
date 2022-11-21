43201bc9f6b880ba9a0131f53fdf247407a67dd7
Harsh Daga <hs108699@gmail.com>
2022-11-21 18:26:08 +0530

    Initial Commit

---
+++ prjct-lkup/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Project from './prjct';
+
+function App() {
+  return (
+    <div className="App">
+      <Project/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ prjct-lkup/src/prjct.js
@@ -0,0 +1,140 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import React from 'react'
+function Project()
+{
+    return(
+        <>
+        <div className="body-6">
+        <div class="navbar-logo-left wf-section">
+            <div data-animation="default" data-collapse="medium" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navbar-logo-left-container shadow-three w-nav">
+                <div class="container-10">
+                    <div class="navbar-wrapper">
+                        <a href="#" class="navbar-brand w-nav-brand"><img src="favicon_web.png" loading="lazy" height="30" sizes="30px" srcset="favicon_web.png, ../images/VateIcon-p-800.png 800w, ../images/VateIcon.png 1024w" alt=""/></a>
+                        <h1 class="heading-153">vate</h1>
+                    </div>
+                </div>
+            </div>
+        </div><div class="project-lookup-div">
+                <div class="project-lookup-container">
+                    <h2 class="heading-154">Click on your project below to view or edit it.</h2>
+                    <div class="lookup-index-container">
+                        <div class="lookup-index">
+                            <div class="lookup-result-item">
+                                <h3 class="heading-155">Submitted to Joe&#x27;s Plumbing</h3>
+                                <div class="project-well">
+                                    <div class="project-well-head"><img src="download-7.png" width="100" height="100" srcset="download-7.png, ../images/download-7.png 640w" sizes="(max-width: 479px) 73vw, (max-width: 767px) 100px, (max-width: 991px) 11vw, 8vw" alt="" class="project-well-thumbnail"/>
+                                        <div class="project-well-head-text">
+                                            <h4 class="project-well-name">Sample Project Name with a really really long title</h4>
+                                            <h5 class="project-type-info">Paint Project in Swoyersville, PA</h5>
+                                            <div class="timeframe-row">
+                                                <h6 class="heading-35">Posted on June 26, 2020</h6>
+                                            </div>
+                                        </div>
+                                        <div class="project-well-head-right">
+                                            <div class="job-owner-name">Wade Boggs</div>
+                                            <div class="job-owner-name">jobOwner@gmail.com</div>
+                                            <div class="job-owner-name">(215) 555-5555</div>
+                                        </div>
+                                    </div>
+                                    <div class="project-well-main">
+                                        <p class="paragraph-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Suspendisse varius enim in eros elementum tristique.Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="lookup-result-item">
+                                <h3 class="heading-155">Submitted to Yo Momma Drywall</h3>
+                                <div class="project-well">
+                                    <div class="project-well-head"><img src="prjct-lkup/public/download-7.png" width="100" height="100" srcset="download-7.png, ../images/download-7.png 640w" sizes="(max-width: 479px) 73vw, (max-width: 767px) 100px, (max-width: 991px) 11vw, 8vw" alt="" class="project-well-thumbnail"/>
+                                        <div class="project-well-head-text">
+                                            <h4 class="project-well-name">Sample Project Name with a really really long title</h4>
+                                            <h5 class="project-type-info">Paint Project in Swoyersville, PA</h5>
+                                            <div class="timeframe-row">
+                                                <h6 class="heading-35">Posted on June 26, 2020</h6>
+                                            </div>
+                                        </div>
+                                        <div class="project-well-head-right">
+                                            <div class="job-owner-name">Wade Boggs</div>
+                                            <div class="job-owner-name">jobOwner@gmail.com</div>
+                                            <div class="job-owner-name">(215) 555-5555</div>
+                                        </div>
+                                    </div>
+                                    <div class="project-well-main">
+                                        <p class="paragraph-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Suspendisse varius enim in eros elementum tristique.Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="lookup-result-item">
+                                <h3 class="heading-155">Submitted to Jims Drywall</h3>
+                                <div class="project-well">
+                                    <div class="project-well-head"><img src="download-7.png" width="100" height="100" srcset="download-7.png, ../images/download-7.png 640w" sizes="(max-width: 479px) 73vw, (max-width: 767px) 100px, (max-width: 991px) 11vw, 8vw" alt="" class="project-well-thumbnail"/>
+                                        <div class="project-well-head-text">
+                                            <h4 class="project-well-name">Sample Project Name with a really really long title</h4>
+                                            <h5 class="project-type-info">Paint Project in Swoyersville, PA</h5>
+                                            <div class="timeframe-row">
+                                                <h6 class="heading-35">Posted on June 26, 2020</h6>
+                                            </div>
+                                        </div>
+                                        <div class="project-well-head-right">
+                                            <div class="job-owner-name">Wade Boggs</div>
+                                            <div class="job-owner-name">jobOwner@gmail.com</div>
+                                            <div class="job-owner-name">(215) 555-5555</div>
+                                        </div>
+                                    </div>
+                                    <div class="project-well-main">
+                                        <p class="paragraph-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Suspendisse varius enim in eros elementum tristique.Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer id="footer" class="footer-2 wf-section">
+                <div class="w-container">
+                    <div class="footer-flex-container">
+                        <div class="div-block-4">
+                            <a href="../old-home.html" class="footer-logo w-inline-block"><img src="favicon_web.png" loading="lazy" width="50" height="50" sizes="(max-width: 479px) 52vw, 50px" srcset="favicon_web.png, ../images/VateIcon-p-800.png 800w, ../images/VateIcon.png 1024w" alt="" class="image-37"/>
+                                <h1 class="heading-7">vate</h1>
+                            </a>
+                            <h4 class="heading-8">Changing Home Improvement for Good</h4>
+                        </div>
+                        <div class="div-block-7">
+                            <h2 class="footer-heading">Vate Resources</h2>
+                            <ul  class="w-list-unstyled">
+                                
+                                    <a href="https://www.vateapp.com/blog/" class="footer-link">Vate Blog</a>
+                                
+                            </ul>
+                        </div>
+                        <div class="div-block-8">
+                            <h2 class="footer-heading">Vate Support</h2>
+                            <ul  class="w-list-unstyled">
+                              
+                                    <a href="../contact-us.html" class="footer-link">Contact Vate</a>
+                               
+                            </ul>
+                        </div>
+                        <div class="div-block-43">
+                            <h2 class="footer-heading">Vate Policies</h2>
+                            
+                            <ul  class="w-list-unstyled">
+                              
+                                    <a href="https://mijovate.s3.amazonaws.com/Vate+Online+Terms+and+Conditions.pdf" target="_blank" class="footer-link" rel="noreferrer">Terms and Conditions</a>
+                              
+                                
+                                    <a href="https://mijovate.s3.amazonaws.com/Vate+Online+Privacy+Policy.pdf" class="footer-link">Privacy Policy</a>
+                               
+                            </ul>
+                            <a href="../what-vate-does-with-your-data.html" class="footer-link">What Vate Does with Your Data</a>
+                        </div>
+                    </div>
+                    <div class="text-block-4">Copyright © 2022 MJV Apps LLC.<br></br>All rights reserved.<br></br>Phone: 1-866-VATEAPP<br></br>Email: info@vateapp.com</div>
+                    </div>
+                    </footer>
+                    </div>
+                    </>
+                    )
+                    }
+                    export default Project
+                    
